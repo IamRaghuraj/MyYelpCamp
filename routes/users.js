1fb2b1df494dd8a4b9f7049b6cacbc0d7c94495e
Raghuraj Thombare <raghurajthombare1997@gmail.com>
2024-03-03 19:31:26 +0530

    Update #10

--- routes/users.js
+++ routes/users.js
@@ -3,32 +3,17 @@ const router = express.Router()
 const passport = require('passport')
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
+const users = require('../controllers/users')
+const { storeReturnTo } = require('../middleware')
 
-router.get('/register', (req, res) => {
-    res.render('users/register')
-})
+router.get('/register', users.renderRegister)
 
-router.post('/register', catchAsync(async(req, res) => {
-    try{
-        const { email, username, password } = req.body
-        const user = new User({ email, username}) 
-        const registeredUser = await User.register(user, password)
-        req.flash("success", "Welcome to Yelp Camp!")
-        res.redirect('/campgrounds')
-    } catch (e) {
-        req.flash('error', e.message)
-        res.redirect('register')
-    }   
-}))
+router.post('/register', catchAsync(users.register))
 
-router.get('/login', (req, res) => {
-    res.render('users/login')
+router.get('/login', users.renderLogin)
 
-})
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }) , users.login)
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }) , (req, res) => {
-    req.flash('success', 'welcome back !')
-    res.redirect('/campgrounds')
-})
+router.get('/logout', users.logout)
 
 module.exports = router
--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
+            author: '65e2e68061be525e446e4901',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
